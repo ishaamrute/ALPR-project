8a4cdf1b1278e7ea99308a64eac39f98b9ce3497
Isha Amrute <38517589+ishaamrute@users.noreply.github.com>
2019-01-16 13:25:42 +1300

    Add files via upload
    Working file

--- index.android.js
+++ index.android.js
@@ -1,3 +1,9 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -5,11 +11,9 @@ import {
   Text,
   View,
   StatusBar,
-  Switch,
-  Picker
 } from 'react-native';
 
-import Camera from './components/camera.js'
+import Camera from 'react-native-openalpr';
 
 const styles = StyleSheet.create({
   container: {
@@ -21,17 +25,9 @@ const styles = StyleSheet.create({
     right: 50,
     bottom: 0,
   },
-  switch: {
-    backgroundColor: '#fff',
-    position: 'absolute',
-    top: 50,
-    left: 50,
-    padding: 10
-  },
   text: {
     textAlign: 'center',
     fontSize: 20,
-    backgroundColor: '#fff',
   },
   preview: {
     flex: 1,
@@ -71,12 +67,6 @@ export default class AwesomeProject extends React.Component {
       camera: {
         aspect: Camera.constants.Aspect.fill,
       },
-      captureQuality: Camera.constants.CaptureQuality.medium,
-      aspect: Camera.constants.CaptureQuality.stretch,
-      rotate: false,
-      torch: false,
-      showBorder: true,
-      color: "#ff0000",
       plate: 'Scan a plate',
     };
   }
@@ -101,14 +91,13 @@ export default class AwesomeProject extends React.Component {
             this.camera = cam;
           }}
           style={styles.preview}
-          aspect={this.state.aspect}
-          captureQuality={this.state.captureQuality}
+          aspect={this.state.camera.aspect}
+          captureQuality={Camera.constants.CaptureQuality.medium}
           country="us"
           onPlateRecognized={this.onPlateRecognized}
-          plateOutlineColor={this.state.color}
-          showPlateOutline={this.state.showBorder}
-          torchMode={this.state.torch ? Camera.constants.TorchMode.on : Camera.constants.TorchMode.off}
-          rotateMode={this.state.rotate ? Camera.constants.RotateMode.on : Camera.constants.RotateMode.off}
+          plateOutlineColor="#ff0000"
+          showPlateOutline
+          torchMode={Camera.constants.TorchMode.off}
           touchToFocus
         />
         <View style={[styles.overlay, styles.topOverlay]}>
@@ -117,47 +106,6 @@ export default class AwesomeProject extends React.Component {
         </View>
         <View style={styles.textContainer}>
           <Text style={styles.text}>{this.state.plate}</Text>
-        </View>
-        <View style={styles.switch}>
-          <Text style={styles.text}>orientation</Text>
-          <Switch
-            onValueChange={(value) => this.setState({rotate: value})}
-            value={this.state.rotate} />
-          <Text style={styles.text}>Torch</Text>
-          <Switch
-            onValueChange={(value) => this.setState({torch: value})}
-            value={this.state.torch} />
-          <Text style={styles.text}>Border</Text>
-          <Switch
-            onValueChange={(value) => this.setState({showBorder: value})}
-            value={this.state.showBorder} />
-          <Text style={styles.text}>Color</Text>
-          <Picker
-            style={{color: '#fff'}}
-            selectedValue={this.state.color}
-            onValueChange={(value) => this.setState({color: value})}>
-              <Picker.Item label="Red" value="#ff0000" />
-              <Picker.Item label="Green" value="#00ff00" />
-              <Picker.Item label="Blue" value="#0000ff" />
-          </Picker>
-          <Text style={styles.text}>Quality</Text>
-          <Picker
-            style={{color: '#fff'}}
-            selectedValue={this.state.captureQuality}
-            onValueChange={(value) => this.setState({captureQuality: value})}>
-              <Picker.Item label="Low" value={Camera.constants.CaptureQuality.low} />
-              <Picker.Item label="Medium" value={Camera.constants.CaptureQuality.medium} />
-              <Picker.Item label="High" value={Camera.constants.CaptureQuality.high} />
-          </Picker>
-          <Text style={styles.text}>Aspect</Text>
-          <Picker
-            style={{color: '#fff'}}
-            selectedValue={this.state.aspect}
-            onValueChange={(value) => this.setState({aspect: value})}>
-              <Picker.Item label="Fill" value={Camera.constants.Aspect.fill} />
-              <Picker.Item label="Fit" value={Camera.constants.Aspect.fit} />
-              <Picker.Item label="Stretch" value={Camera.constants.Aspect.stretch} />
-          </Picker>
         </View>
       </View>
 
